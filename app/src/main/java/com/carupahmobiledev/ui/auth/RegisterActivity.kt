9095014a3dfdab5bf316c1349cbaa62ce125a5f6
RaihanFadhlal <raihanfadhlal@gmail.com>
2023-05-31 23:35:48 +0700

    register view

---
+++ app/src/main/java/com/carupahmobiledev/ui/auth/RegisterActivity.kt
@@ -0,0 +1,12 @@
+package com.carupahmobiledev.ui.auth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.carupahmobiledev.R
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
